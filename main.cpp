0735565f47467c1bf8bdd4ad757bdc55db18d2bf
Gregor A <gregor.alic@gimb.org>
2022-07-02 18:00:29 +0200

    Create render() function

--- main.cpp
+++ main.cpp
@@ -3,10 +3,18 @@
 
 using namespace std;
 
+void render(Arm a, float time);
+
 int main(){
 
 	vector<float> sizes = {1, 1, 1};
 	Arm a(sizes);
+
+	render(a, 20);
+
+}
+
+void render(Arm a, float time){
 
 	int WIDTH = 800;
 	int HEIGHT = 600;
@@ -17,27 +25,33 @@ int main(){
 	view.zoom(0.03);
 	window.setView(view);
 
+	sf::Clock clock;
+	float passed = 0;
 	while (window.isOpen())
 	{
-	// check all the window's events that were triggered since the last iteration of the loop
-	sf::Event event;
-	while (window.pollEvent(event))
-	{
-		// "close requested" event: we close the window
-		if (event.type == sf::Event::Closed)
+		window.clear(sf::Color::Black);
+
+		sf::Time delta = clock.restart();
+		a.physics(delta.asSeconds(), true);
+
+		window.draw(a);
+
+		// end the current frame
+		window.display();
+
+
+		passed+=delta.asSeconds();
+		if(passed >= time){
 			window.close();
+		}
+
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
 	}
-
-	// clear the window with black color
-	window.clear(sf::Color::Black);
-
-	a.physics(1.0f / 60.0f, true);
-	// draw everything here...
-	// window.draw(...);
-	window.draw(a);
-
-	// end the current frame
-	window.display();
-}
 
 }
