6c4b067bb4661cbee458de0ec6dcec8c1855aa7c
Gregor A <gregor.alic@gimb.org>
2022-07-10 19:37:28 +0200

    Example code now approximates using local derivatives

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 #include "include/arm.h"
 #include <stdio.h>
 #include <iostream>
+#include <random>
 
 using namespace std;
 
 void render(Arm a);
+
+float randFloat(){
+	std::random_device rd{};
+	std::mt19937 engine{rd()};
+	std::uniform_real_distribution<double> dist{0.0, 1.0};
+	double ret = dist(engine);
+	return ret;
+}
+
 
 int main(){
 
@@ -30,14 +40,99 @@ void render(Arm a){
 
 	sf::Clock clock;
 	float passed = 0;
+
+	float tx = 1;
+	float ty = 1;
+
 	while (window.isOpen())
 	{
 		window.clear(sf::Color::Black);
 
+		sf::Vector2i pixelPos = sf::Mouse::getPosition(window);
+		sf::Vector2f worldPos = window.mapPixelToCoords(pixelPos);
+
+
+		if(sf::Mouse::isButtonPressed(sf::Mouse::Left)){
+			tx = worldPos.x;
+			ty = -worldPos.y;
+		}
+
+		float angles[] = {0,0,0};
+		a.getAngles(&angles[0]);
+
+		float a1,a2,a3;
+
+		float dx = (tx - a.getArmLocation().x);
+		float dy = (ty - a.getArmLocation().y);
+		float dlen = sqrt(pow(dx, 2) + pow(dy, 2));
+
+		dx = dx / dlen;
+		dy = dy / dlen;
+
+		float min = 100000;
+
+		float drx,dry;
+		float drlen;
+
+		for(int i = 0; i < 100; i++){
+			float ar1 = (randFloat() - 0.5);
+			float ar2 = (randFloat() - 0.5);
+			float ar3 = (randFloat() - 0.5);
+
+			drx = -ar1*sin(angles[2] + 3.14/2)
+						-(ar1 + ar2)*sin(angles[2] + angles[1] + 3.14/2)
+						-(ar1 + ar2 + ar3)*sin(angles[2] + angles[1] + angles[0] + 3.14/2);
+
+			dry = ar1*cos(angles[2] + 3.14/2)
+						+(ar1 + ar2)*cos(angles[2] + angles[1] + 3.14/2)
+						+(ar1 + ar2 + ar3)*cos(angles[2] + angles[1] + angles[0] + 3.14/2);
+
+			drlen = sqrt(pow(drx, 2) + pow(dry, 2));
+			drx = drx / drlen;
+			dry = dry / drlen;
+
+			float val = pow(drx - dx, 2) + pow(dry - dy, 2);
+			if(val < min){
+				min = val;
+				a1 = ar1;
+				a2 = ar2;
+				a3 = ar3;
+			}
+
+		}
+
+		//printf("D: %f %f\n", dx, dy);
+		//printf("DR: %f %f\n", drx, dry);
+		//printf("ERR: %f\n", sqrt(pow(drx - dx, 2) + pow(dry - dy, 2)));
+
+		if(sf::Mouse::isButtonPressed(sf::Mouse::Right)){
+			a1 = -5*(angles[0]);
+			a2 = -5*(angles[1]);
+			a3 = -5*(angles[2]);
+		}
+
+		a.applySpeeds({a1, a2, a3});
+
 		sf::Time delta = clock.restart();
 		a.physics(delta.asSeconds(), false);
+		//a.physics(1.0f/30, false);
 
+		sf::RectangleShape dir;
+		dir.setSize(sf::Vector2f(0.2, 0.02));
+		dir.setOrigin(0, 0.01);
+		dir.setPosition(a.getArmLocation().x, -a.getArmLocation().y);
+		dir.setFillColor(sf::Color::Red);
+		dir.setRotation(atan(abs(drx) / dry) * 180/3.14);
+
+		sf::CircleShape target(0.05);
+		target.setOrigin(0.05, 0.05);
+		target.setFillColor(sf::Color(255, 0, 0));
+
+		target.setPosition(tx, -ty);
+
+		window.draw(target);
 		window.draw(a);
+		//window.draw(dir);
 
 		// end the current frame
 		window.display();
